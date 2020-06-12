60386916a749b571acb3cc8d333be6debdae92ed
Catherine Tucker <ImCatherineNoel@gmail.com>
2020-06-11 18:57:02 -0700

    Task 1 and 2 done

--- index.js
+++ index.js
@@ -41,8 +41,27 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+  this.name = name;
+  this.age= age;
+  this.stomach = [ ];
+  };
+  eat(food){
+       if (this.stomach.length < 10) {
+          this.stomach.push(food);
+       }
+       else {
+            return(`You need to poop first!`);
+       }
+  };
+  poop() {
+       this.stomach = [ ];
+   }; 
+  toString(){
+    return `${this.name}, ${this.age}`;
+  };
+};
 
-}
 
 /*
   TASK 2
@@ -59,7 +78,28 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon= milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+    };
+   fill(gallons) {
+     this.tank = this.tank + gallons;
+   }; 
+   drive(distance){
+      if ((this.tank * this.milesPerGallon) - distance <0 ) {
+        this.odometer = this.odometer + this.tank * this.milesPerGallon;
+        this.tank =0
+        return(`Ran out of fuel at ${this.odometer}miles!"`)
+      }
+      else{
+       this.odometer = this.odometer + distance;
+       this.tank = this.tank - (distance / this.milesPerGallon);        
+      }
+      
 
+   }
 }
 
 /*
